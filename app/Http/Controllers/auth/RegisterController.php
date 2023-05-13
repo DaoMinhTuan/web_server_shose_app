b5f6017013daebe8b2c239ab559effd811ddc973
zuzhonz <hoangxuanvu248@gmail.com>
2023-05-13 13:14:02 +0700

    fix

--- app/Http/Controllers/auth/RegisterController.php
+++ app/Http/Controllers/auth/RegisterController.php
@@ -37,97 +37,98 @@ class RegisterController extends Controller
     {
 
         $data = new User;
-
-        $user = $data->where('token', $token)->first();
-
-        $users = User::find($user->id);
-        $users->token = "activation";
-        $users->status = 1;
-
         try {
+            $user = $data->where('token', $token)->first();
+
+            $users = User::find($user->id);
+            $users->token = "activation";
+            $users->status = 1;
             $users->save();
         } catch (\Exception $err) {
-            return response()->json([
-                'status' => '400',
-                'message' => "creat at user not successfully"
-            ]);
+            return redirect('https://mail.google.com/mail/u/0/#inbox');
         }
         return redirect('https://mail.google.com/mail/u/0/#inbox');
     }
 
     public function api_register(ApiLoginRequest $request)
-    {
-
+    {   
+       
         $validated = $request->safe()->merge([
-            'address_id' => 0,
-            'role_id' => 1,
-            'status' => 0,
-            'token' => md5(uniqid(rand(), true)),
-            'avatar' => " no avatar available",
-            'password' => Hash::make($request->password),
-        ]);
-        $users = new User;
-        $users->fill($validated->all());
-
+                'address_id' => 0,
+                'role_id' => 1,
+                'status' => 0,
+                'token' => md5(uniqid(rand(), true)),
+                'avatar' => " no avatar available",
+                'password' => Hash::make($request->password),
+            ]);
+        
+        
+        
         try {
+
+            $users = new User;
+            $users->fill($validated->all());
+
+            
             $users->save();
+
+            $data = [
+                'subject' => 'Shose App',
+                'body' => 'Welcome to Shose App',
+                'token' => $validated['token'],
+            ];
+            Mail::to($validated['email'])->send(new MailNotify($data));
+
+            return response()->json([
+                'status' => '200',
+                'message' => 'created successfully'
+            ]);
+            
         } catch (\Exception $err) {
             return response()->json([
                 'status' => '400',
                 'message' => "creat at user not successfully"
             ]);
         }
-
-
-        $data = [
-            'subject' => 'Shose App',
-            'body' => 'Welcome to Shose App',
-            'token' => $validated['token'],
-        ];
-        Mail::to($validated['email'])->send(new MailNotify($data));
-
-        return response()->json([
-            'status' => '200',
-            'message' => 'created successfully'
-        ]);
     }
 
     public function api_admin_register(ApiLoginRequest $request)
-    {
-
+    {   
+       
         $validated = $request->safe()->merge([
-            'address_id' => 0,
-            'role_id' => 2,
-            'status' => 0,
-            'token' => md5(uniqid(rand(), true)),
-            'avatar' => " no avatar available",
-            'password' => Hash::make($request->password),
+                'address_id' => 0,
+                'role_id' => 2,
+                'status' => 0,
+                'token' => md5(uniqid(rand(), true)),
+                'avatar' => " no avatar available",
+                'password' => Hash::make($request->password),
         ]);
-
-
-        $users = new User;
-        $users->fill($validated->all());
-
+        
         try {
+
+            $users = new User;
+            $users->fill($validated->all());
+
+            
             $users->save();
+
+            $data = [
+                'subject' => 'Shose App',
+                'body' => 'Welcome to Shose App',
+                'token' => $validated['token'],
+            ];
+            Mail::to($validated['email'])->send(new MailNotify($data));
+
+            return response()->json([
+                'status' => '200',
+                'message' => 'created successfully'
+            ]);
+            
         } catch (\Exception $err) {
             return response()->json([
                 'status' => '400',
                 'message' => "creat at user not successfully"
             ]);
         }
-
-
-        $data = [
-            'subject' => 'Shose App',
-            'body' => 'Welcome to Shose App',
-            'token' => $validated['token'],
-        ];
-        Mail::to($validated['email'])->send(new MailNotify($data));
-
-        return response()->json([
-            'status' => '200',
-            'message' => 'created successfully'
-        ]);
     }
 }
