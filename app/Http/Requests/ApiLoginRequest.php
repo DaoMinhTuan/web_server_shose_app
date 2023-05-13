c4b8edd8ebf0fbf49a4957f61e3db8c1d554d110
zuzhonz <hoangxuanvu248@gmail.com>
2023-05-13 12:43:59 +0700

    fis

--- app/Http/Requests/ApiLoginRequest.php
+++ app/Http/Requests/ApiLoginRequest.php
@@ -54,13 +54,14 @@ class ApiLoginRequest extends FormRequest
                             'password' => 'required',
                         ];
                         break;
-                    case 'api_register':
-                        $rules = [
-                            'name' => 'required',
-                            'email' => 'required|email|unique:users,email',
-                            'password' => 'required',
-                        ];
-                        break;
+                   
+                    case 'api_admin_register':
+                            $rules = [
+                                'name' => 'required',
+                                'email' => 'required|email|unique:users,email',
+                                'password' => 'required',
+                            ];
+                            break;
                     case 'send_otp':
                         $rules = [
                             'email' => 'required|email',
